e3d67d380179b22ed46aa7d34f7292a31ae7f487
Magnus Lindstrom <magnus.lindstrom@tuta.io>
2022-12-09 22:17:38 +0100

    make input fetching script fetch all

--- get_daily_input.sh
+++ get_daily_input.sh
@@ -2,34 +2,29 @@
 
 set -e
 
-usage() {
-  echo """
-Fetch the input of a certain day of aoc2022
-
-Usage: get_daily_input.sh <day_number>
-
-Optional arguments:
-  -h, --help   Display this message and exit.
-"""
-  exit $1
-}
-
-
-POSITIONAL=()
-while [[ $# -gt 0 ]]; do
-  key="$1"
-  case $key in
-    -h|--help) usage 0; shift ;;
-    -*) usage 1; shift ;;
-    *)  POSITIONAL+=("$1"); shift ;;
-  esac
-done
-
-set -- "${POSITIONAL[@]}" # restore positional parameters
-
-if [ -z $1 ]; then
-  usage 1
+if ! [ -d ./.git ]; then
+  echo "Must be run from git root."
+  exit 1
 fi
 
-day_nr=$1
-curl -H "Cookie: session=$(cat .session_cookie)" -o "inputs/day${day_nr}.txt" "https://adventofcode.com/2022/day/${day_nr}/input"
+if ! [ -f ./.session_cookie ]; then
+  echo "No .session_cookie file defined. See README.md."
+  exit 1
+fi
+
+mkdir -p inputs
+
+for day in $(seq 1 25); do
+  if ! [ -f "./inputs/day${day}.txt" ]; then
+    curl --silent -H "Cookie: session=$(cat .session_cookie)" \
+      -o "inputs/day${day}.txt" \
+      "https://adventofcode.com/2022/day/${day}/input"
+    if [ "$(head -n1 inputs/day${day}.txt | cut -d ' ' -f 1,2)" = "Please don't" ]; then
+      echo "Day ${day} and beyond have not been published yet."
+      rm "inputs/day${day}.txt"
+      break
+    fi
+    echo "Fetched day ${day} input."
+  fi
+done
+echo "Done."
