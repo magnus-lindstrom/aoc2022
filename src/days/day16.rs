b61eba3514d5d0912aa58e9af9cc02bb36ca2633
Magnus Lindstrom <magnus.lindstrom@tuta.io>
2022-12-15 18:49:27 +0100

    add some termio

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -1,6 +1,33 @@
+extern crate termion;
+
+use std::io::{stdout, Write};
+use termion::{color, style};
+
 const _FILE_PATH: &str = "inputs/dayX.txt";
 
 pub fn result_a() -> Result<i32, &'static str> {
+    let mut stdout = stdout();
+    println!("{}Red", color::Fg(color::Red));
+    println!("{}Blue", color::Fg(color::Blue));
+    println!("{}Blue'n'Bold{}", style::Bold, style::Reset);
+    println!("{}Just plain italic", style::Italic);
+
+    print!(
+        "{}{}hello",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1)
+    );
+    stdout.flush().unwrap();
+    std::thread::sleep(std::time::Duration::from_millis(1000));
+    print!(
+        "{}{}there",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1)
+    );
+    std::thread::sleep(std::time::Duration::from_millis(1000));
+    print!("{}{}foo", termion::clear::All, termion::cursor::Goto(1, 1));
+    std::thread::sleep(std::time::Duration::from_millis(1000));
+    print!("{}{}bar", termion::clear::All, termion::cursor::Goto(1, 1));
     Ok(0)
 }
 
