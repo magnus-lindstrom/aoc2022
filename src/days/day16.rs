07dcd758450d7cbdabe1bdff26086c6d9efef00f
Magnus Lindstrom <magnus.lindstrom@tuta.io>
2022-12-16 21:13:43 +0100

    begin work on 16b, prior to i32->char rewrite

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -1,8 +1,8 @@
 use crate::utils;
 use sorted_vec::SortedVec;
 use std::collections::HashMap;
-const FILE_PATH: &str = "inputs/day16.txt";
-//const FILE_PATH: &str = "inputs/day16_test.txt";
+// const FILE_PATH: &str = "inputs/day16.txt";
+const FILE_PATH: &str = "inputs/day16_test.txt";
 
 /// function to translate node strings, e.g. "AA" to a number
 fn bigram_to_i32(bigram: &str) -> i32 {
@@ -168,12 +168,12 @@ fn naive_solution() -> Result<i32, &'static str> {
     Err("did not run to completion")
 }
 
-/*
 pub fn result_b() -> Result<i32, &'static str> {
     let input = utils::vector_of_string_vectors_from_file(FILE_PATH);
     // node name to flow rate and connected nodes
     let mut node_connections: HashMap<i32, Vec<i32>> = HashMap::new();
     let mut node_flows: HashMap<i32, i32> = HashMap::new();
+    let time_until_collapse: usize = 26;
 
     for line in input.iter() {
         let node_name = &line[1];
@@ -195,7 +195,7 @@ pub fn result_b() -> Result<i32, &'static str> {
     for flow in node_flows.values() {
         total_flow += flow;
     }
-    let max_released_volume = 29 * total_flow;
+    let max_released_volume = (time_until_collapse as i32 - 1) * total_flow;
 
     // first number is the total blocked volume so far. The lower the better.
     // second number is the open flow. The higher the better.
@@ -203,8 +203,8 @@ pub fn result_b() -> Result<i32, &'static str> {
     // fourth element, the second vector, is the path taken of me.
     let mut paths_to_explore: SortedVec<(i32, i32, Vec<i32>, Vec<i32>)> = SortedVec::new();
     let aa_connections = &node_connections[&bigram_to_i32("AA")];
-    let elephant_possibilities: Vec<i32> = Vec::new();
-    let my_possibilities: Vec<i32> = Vec::new();
+    let mut elephant_possibilities: Vec<i32> = Vec::new();
+    let mut my_possibilities: Vec<i32> = Vec::new();
     for node in aa_connections.into_iter() {
         my_possibilities.push(*node);
         elephant_possibilities.push(*node);
@@ -229,35 +229,56 @@ pub fn result_b() -> Result<i32, &'static str> {
         let path_len = my_path.len();
         if path_len == max_iter {
             println!("path length: {}", path_len);
-            max_iter += 5;
+            max_iter += 1;
         }
-        if my_path.len() == 30 {
+        if my_path.len() == time_until_collapse {
             return Ok(max_released_volume - total_blocked_volume);
         }
 
-        for node in &node_connections[node_path.last().unwrap()] {
-            let mut node_path_plus_new_node = node_path.clone();
-            node_path_plus_new_node.push(*node);
-            paths_to_explore.insert((
-                total_blocked_volume + (total_flow - current_flow),
-                current_flow,
-                node_path_plus_new_node,
+        let mut elephant_possibilities_with_additional_flow: Vec<(i32, i32)> = Vec::new();
+        for node in &node_connections[ele_path.last().unwrap()] {
+            elephant_possibilities_with_additional_flow.push((*node, 0));
+        }
+        if node_flows[ele_path.last().unwrap()] > 0 && last_node_not_opened_in_path(&ele_path) {
+            elephant_possibilities_with_additional_flow.push((
+                *ele_path.last().unwrap(),
+                node_flows[ele_path.last().unwrap()],
             ));
         }
-        if node_flows[node_path.last().unwrap()] > 0 && last_node_not_opened_in_path(&node_path) {
-            let mut node_path_plus_new_node = node_path.clone();
-            node_path_plus_new_node.push(*node_path.last().unwrap());
-            let new_flow = current_flow + node_flows[node_path.last().unwrap()];
-            paths_to_explore.insert((
-                total_blocked_volume + (total_flow - new_flow),
-                new_flow,
-                node_path_plus_new_node,
+
+        let mut my_possibilities_with_additional_flow: Vec<(i32, i32)> = Vec::new();
+        for node in &node_connections[my_path.last().unwrap()] {
+            my_possibilities_with_additional_flow.push((*node, 0));
+        }
+        if node_flows[my_path.last().unwrap()] > 0 && last_node_not_opened_in_path(&my_path) {
+            my_possibilities_with_additional_flow.push((
+                *my_path.last().unwrap(),
+                node_flows[my_path.last().unwrap()],
             ));
         }
+
+        for (my_pos, my_added_flow) in my_possibilities_with_additional_flow.iter() {
+            for (ele_pos, ele_added_flow) in elephant_possibilities_with_additional_flow.iter() {
+                let mut my_node_path_plus_new_node = my_path.clone();
+                my_node_path_plus_new_node.push(*my_pos);
+                let mut ele_node_path_plus_new_node = ele_path.clone();
+                ele_node_path_plus_new_node.push(*ele_pos);
+
+                paths_to_explore.insert((
+                    total_blocked_volume + total_flow
+                        - current_flow
+                        - my_added_flow
+                        - ele_added_flow,
+                    current_flow + my_added_flow + ele_added_flow,
+                    ele_node_path_plus_new_node,
+                    my_node_path_plus_new_node,
+                ));
+            }
+        }
+        println!("length of possible next steps: {}", paths_to_explore.len());
     }
     Err("did not run to completion")
 }
-*/
 
 /*
 #[cfg(test)]
