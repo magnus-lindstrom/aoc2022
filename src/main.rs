a06c9112c3da9b7e6a51d0b10e05e6a8931117e8
Magnus Lindstrom <magnus.lindstrom@capgemini.com>
2022-12-20 16:14:02 +0100

    slightly faster day19

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ pub mod days;
 mod utils;
 
 fn main() -> () {
-    println!("day 20 answer a:\n{}", days::day20::result_a().unwrap());
-    println!("\nday 20 answer b:\n{}", days::day20::result_b().unwrap());
+    println!("Part a:\n{}", days::day19::result_a().unwrap());
+    println!("\nPart b:\n{}", days::day19::result_b().unwrap());
 }
