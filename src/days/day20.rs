a2e2bd946b835d516cb5965b26a9fa9b7f23f9e9
Magnus Lindstrom <magnus.lindstrom@capgemini.com>
2022-12-20 11:57:58 +0100

    add day20b

--- src/days/day20.rs
+++ src/days/day20.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
 const FILE_PATH: &str = "inputs/day20.txt";
 //const FILE_PATH: &str = "inputs/day20_test.txt";
+//const FILE_PATH: &str = "inputs/day20_scratch.txt";
 
-fn get_new_pos_of_nr(a: i32, mut b: i32, input_len: i32) -> i32 {
-    let new_place: i32;
-    if a + b <= -input_len {
-        b += input_len;
-    }
+fn get_new_pos_of_nr(a: i64, mut b: i64, input_len: i64) -> i64 {
+    let new_place: i64;
+
+    b = b % (input_len - 1);
 
     if a + b <= 0 {
         new_place = (a + b + input_len - 1) % input_len;
@@ -19,11 +19,11 @@ fn get_new_pos_of_nr(a: i32, mut b: i32, input_len: i32) -> i32 {
 }
 
 fn get_affected_positions_and_if_moving_to_the_right(
-    a: i32,
-    b: i32,
-    input_len: i32,
-) -> (Vec<i32>, bool) {
-    let intermediate_positions: Vec<i32>;
+    a: i64,
+    b: i64,
+    input_len: i64,
+) -> (Vec<i64>, bool) {
+    let intermediate_positions: Vec<i64>;
     let moving_to_the_right: bool;
 
     let new_pos = get_new_pos_of_nr(a, b, input_len);
@@ -38,18 +38,18 @@ fn get_affected_positions_and_if_moving_to_the_right(
     return (intermediate_positions, moving_to_the_right);
 }
 
-fn get_new_order(old_vec: &Vec<i32>, old_pos_to_new_pos: &HashMap<i32, i32>) -> Vec<i32> {
-    let mut new_vec: Vec<i32> = vec![0; old_vec.len()];
+fn get_new_order(old_vec: &Vec<i64>, old_pos_to_new_pos: &HashMap<i64, i64>) -> Vec<i64> {
+    let mut new_vec: Vec<i64> = vec![0; old_vec.len()];
     for i in 0..old_vec.len() {
         let nr = old_vec[i];
-        let new_pos = old_pos_to_new_pos[&(i as i32)];
+        let new_pos = old_pos_to_new_pos[&(i as i64)];
         new_vec[new_pos as usize] = nr;
     }
 
     new_vec
 }
 
-fn get_num_x_positions_after_0(vec: &Vec<i32>, x: usize) -> i32 {
+fn get_num_x_positions_after_0(vec: &Vec<i64>, x: usize) -> i64 {
     let vec_len = vec.len();
     let mut pos_0: Option<usize> = None;
     for i in 0..vec_len {
@@ -63,42 +63,35 @@ fn get_num_x_positions_after_0(vec: &Vec<i32>, x: usize) -> i32 {
     }
 }
 
-pub fn result_a() -> Result<i32, &'static str> {
-    /*
-    println!(
-    "affected_positions: {:?}",
-    get_affected_positions(2, -5, 20)
-    );
-    return Ok(0);
-    */
-    let input: Vec<i32> = crate::utils::nr_vec_from_file_allow_empty_lines(FILE_PATH);
-    let input_len = input.len() as i32;
-    let mut old_pos_to_new_pos: HashMap<i32, i32> = HashMap::new();
-    let mut new_pos_to_old_pos: HashMap<i32, i32> = HashMap::new();
+pub fn result_a() -> Result<i64, &'static str> {
+    let input: Vec<i64> = crate::utils::nr_vec_from_file_allow_empty_lines(FILE_PATH);
+    let input_len = input.len();
+    let mut old_pos_to_new_pos: HashMap<i64, i64> = HashMap::new();
+    let mut new_pos_to_old_pos: HashMap<i64, i64> = HashMap::new();
     for (i, _) in input.iter().enumerate() {
-        old_pos_to_new_pos.insert(i as i32, i as i32);
-        new_pos_to_old_pos.insert(i as i32, i as i32);
+        old_pos_to_new_pos.insert(i as i64, i as i64);
+        new_pos_to_old_pos.insert(i as i64, i as i64);
     }
     for (prior_pos, item) in input.iter().enumerate() {
-        let current_pos_in_list = old_pos_to_new_pos[&(prior_pos as i32)];
-        let updated_position = get_new_pos_of_nr(current_pos_in_list, *item, input_len);
+        let current_pos_in_list = old_pos_to_new_pos[&(prior_pos as i64)];
+        let updated_position = get_new_pos_of_nr(current_pos_in_list, *item, input_len as i64);
         let (intermediate_positions, moving_to_the_right) =
             get_affected_positions_and_if_moving_to_the_right(
-                old_pos_to_new_pos[&(prior_pos as i32)],
+                old_pos_to_new_pos[&(prior_pos as i64)],
                 *item,
-                input_len,
+                input_len as i64,
             );
-        let update_intermediates_with: i32;
+        let update_intermediates_with: i64;
         if moving_to_the_right {
             update_intermediates_with = -1;
         } else {
             update_intermediates_with = 1;
         }
 
-        //println!("intermediate_positions: {:?}", intermediate_positions);
         for inter_pos in intermediate_positions.iter() {
             // moving all numbers affected by the actual number move
-            let new_inter_pos = (inter_pos + input_len + update_intermediates_with) % input_len;
+            let new_inter_pos =
+                (inter_pos + input_len as i64 + update_intermediates_with) % input_len as i64;
             let input_pos = new_pos_to_old_pos[&inter_pos];
             new_pos_to_old_pos.insert(new_inter_pos, input_pos);
 
@@ -107,13 +100,13 @@ pub fn result_a() -> Result<i32, &'static str> {
         }
 
         // now update the nr in question
-        old_pos_to_new_pos.insert(prior_pos as i32, updated_position);
-        new_pos_to_old_pos.insert(updated_position, prior_pos as i32);
+        old_pos_to_new_pos.insert(prior_pos as i64, updated_position);
+        new_pos_to_old_pos.insert(updated_position, prior_pos as i64);
+        //println!("{:?}", get_new_order(&input, &old_pos_to_new_pos));
     }
     let new_order = get_new_order(&input, &old_pos_to_new_pos);
-    println!("{:?}", new_order);
 
-    let mut result_sum: i32 = 0;
+    let mut result_sum: i64 = 0;
     result_sum += get_num_x_positions_after_0(&new_order, 1000);
     result_sum += get_num_x_positions_after_0(&new_order, 2000);
     result_sum += get_num_x_positions_after_0(&new_order, 3000);
@@ -121,8 +114,61 @@ pub fn result_a() -> Result<i32, &'static str> {
     Ok(result_sum)
 }
 
-pub fn result_b() -> Result<i32, &'static str> {
-    Ok(0)
+pub fn result_b() -> Result<i64, &'static str> {
+    let mut input: Vec<i64> = crate::utils::nr_vec_from_file_allow_empty_lines(FILE_PATH);
+    let magic_nr: i64 = 811589153;
+    let input_len = input.len();
+    for i in 0..input_len {
+        input[i] *= magic_nr;
+    }
+    let mut old_pos_to_new_pos: HashMap<i64, i64> = HashMap::new();
+    let mut new_pos_to_old_pos: HashMap<i64, i64> = HashMap::new();
+    for (i, _) in input.iter().enumerate() {
+        old_pos_to_new_pos.insert(i as i64, i as i64);
+        new_pos_to_old_pos.insert(i as i64, i as i64);
+    }
+    let mut new_order = input.clone();
+    for _ in 0..10 {
+        for (prior_pos, item) in input.iter().enumerate() {
+            let current_pos_in_list = old_pos_to_new_pos[&(prior_pos as i64)];
+            let updated_position = get_new_pos_of_nr(current_pos_in_list, *item, input_len as i64);
+            let (intermediate_positions, moving_to_the_right) =
+                get_affected_positions_and_if_moving_to_the_right(
+                    old_pos_to_new_pos[&(prior_pos as i64)],
+                    *item,
+                    input_len as i64,
+                );
+            let update_intermediates_with: i64;
+            if moving_to_the_right {
+                update_intermediates_with = -1;
+            } else {
+                update_intermediates_with = 1;
+            }
+
+            for inter_pos in intermediate_positions.iter() {
+                // moving all numbers affected by the actual number move
+                let new_inter_pos =
+                    (inter_pos + input_len as i64 + update_intermediates_with) % input_len as i64;
+                let input_pos = new_pos_to_old_pos[&inter_pos];
+                new_pos_to_old_pos.insert(new_inter_pos, input_pos);
+
+                // update old to new position map
+                old_pos_to_new_pos.insert(input_pos, new_inter_pos);
+            }
+
+            // now update the nr in question
+            old_pos_to_new_pos.insert(prior_pos as i64, updated_position);
+            new_pos_to_old_pos.insert(updated_position, prior_pos as i64);
+        }
+        new_order = get_new_order(&input, &old_pos_to_new_pos);
+    }
+
+    let mut result_sum: i64 = 0;
+    result_sum += get_num_x_positions_after_0(&new_order, 1000);
+    result_sum += get_num_x_positions_after_0(&new_order, 2000);
+    result_sum += get_num_x_positions_after_0(&new_order, 3000);
+
+    Ok(result_sum)
 }
 
 /*
