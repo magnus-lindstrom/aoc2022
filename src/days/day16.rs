6d3aa749ba72c5433328601e4d0ad16cd1cea6de
Magnus Lindstrom <magnus.lindstrom@tuta.io>
2022-12-22 18:12:16 +0100

    start making small changes in day16

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -2,12 +2,46 @@ use crate::utils;
 use sorted_vec::SortedVec;
 use std::collections::HashMap;
 const FILE_PATH: &str = "inputs/day16.txt";
-//const FILE_PATH: &str = "inputs/day16_test.txt";
+const TEST_FILE_PATH: &str = "inputs/day16_test.txt"; // a: 1651. b: 1707
+const MODE: &str = "real"; // real or test
 
-/// function to translate node strings, e.g. "AA" to a number
-fn bigram_to_i32(bigram: &str) -> i32 {
-    1000 * bigram.as_bytes()[0] as i32 + bigram.as_bytes()[1] as i32
+fn get_input() -> (
+    HashMap<(char, char), Vec<(char, char)>>,
+    HashMap<(char, char), i32>,
+) {
+    let input: Vec<Vec<String>>;
+    if MODE == "real" {
+        input = utils::vector_of_string_vectors_from_file(FILE_PATH);
+    } else if MODE == "test" {
+        input = utils::vector_of_string_vectors_from_file(TEST_FILE_PATH);
+    } else {
+        panic!("unimplemented mode");
+    }
+    // node name to flow rate and connected nodes
+    let mut node_connections: HashMap<(char, char), Vec<(char, char)>> = HashMap::new();
+    let mut node_flows: HashMap<(char, char), i32> = HashMap::new();
+
+    for line in input.iter() {
+        let node_name = &line[1];
+        let node_name_char1: char = node_name.chars().next().unwrap();
+        let node_name_char2: char = node_name.chars().nth(1).unwrap();
+        let flow_rate: i32 = line[4]
+            .trim_end_matches(';')
+            .trim_start_matches(&['r', 'a', 't', 'e', '='])
+            .parse()
+            .unwrap();
+        let mut connected_node_names: Vec<(char, char)> = Vec::new();
+        for connected_name in 9..line.len() {
+            let char1: char = line[connected_name].chars().next().unwrap();
+            let char2: char = line[connected_name].chars().nth(1).unwrap();
+            connected_node_names.push((char1, char2));
+        }
+        node_flows.insert((node_name_char1, node_name_char2), flow_rate);
+        node_connections.insert((node_name_char1, node_name_char2), connected_node_names);
+    }
+    (node_connections, node_flows)
 }
+
 fn node_not_opened_in_either_path(
     node: (char, char),
     node_path_1: &Vec<(char, char)>,
@@ -26,7 +60,7 @@ fn node_not_opened_in_either_path(
     true
 }
 
-fn last_node_not_opened_in_path(node_path: &Vec<i32>) -> bool {
+fn last_node_not_opened_in_path(node_path: &Vec<(char, char)>) -> bool {
     for i in 1..node_path.len() {
         if node_path[i] == *node_path.last().unwrap()
             && node_path[i - 1] == *node_path.last().unwrap()
@@ -37,42 +71,42 @@ fn last_node_not_opened_in_path(node_path: &Vec<i32>) -> bool {
     true
 }
 
+#[allow(dead_code)]
+fn print_char_path(char_path: &Vec<(char, char)>) -> () {
+    for node in char_path.iter() {
+        print!("{}{}-", node.0, node.1);
+    }
+    println!("");
+}
+
+/*
+fn get_pruned_connections(
+    node_connections: &mut HashMap<(char, char), Vec<(char, char)>>,
+    node_flows: HashMap<(char, char), i32>,
+) -> HashMap<(char, char), i32> {
+    let mut connections_and_step_length: HashMap<(char, char), i32>;
+
+    connections_and_step_length
+}
+*/
+
 pub fn result_a() -> Result<i32, &'static str> {
-    let input = utils::vector_of_string_vectors_from_file(FILE_PATH);
-    // node name to flow rate and connected nodes
-    let mut node_connections: HashMap<i32, Vec<i32>> = HashMap::new();
-    let mut node_flows: HashMap<i32, i32> = HashMap::new();
-    let mut total_flow: i32 = 0;
+    let (node_connections, node_flows) = get_input();
     let cutoff_length = 500;
 
-    // first number is the total blocked volume so far. The lower the better.
-    // second number is the open flow. The higher the better.
-    // third element, the vector, is the path taken.
-    let mut paths_to_explore: SortedVec<(i32, i32, Vec<i32>)> = SortedVec::new();
-    for line in input.iter() {
-        let node_name = &line[1];
-        let flow_rate: i32 = line[4]
-            .trim_end_matches(';')
-            .trim_start_matches(&['r', 'a', 't', 'e', '='])
-            .parse()
-            .unwrap();
-        let mut connected_node_names: Vec<i32> = Vec::new();
-        for connected_name in 9..line.len() {
-            connected_node_names.push(bigram_to_i32(line[connected_name].trim_end_matches(',')));
-        }
-        let node_name_code = bigram_to_i32(node_name);
-        node_flows.insert(node_name_code, flow_rate);
-        node_connections.insert(node_name_code, connected_node_names);
-    }
+    let mut total_flow: i32 = 0;
     for flow in node_flows.values() {
         total_flow += flow;
     }
     let max_released_volume = 29 * total_flow;
-    let aa_connections = &node_connections[&bigram_to_i32("AA")];
+    let aa_connections = &node_connections[&('A', 'A')];
+
+    // first number is the total blocked volume so far. The lower the better.
+    // second number is the open flow. The higher the better.
+    // third element, the vector, is the path taken.
+    let mut paths_to_explore: SortedVec<(i32, i32, Vec<(char, char)>)> = SortedVec::new();
     for node in aa_connections.into_iter() {
-        let new_path = vec![bigram_to_i32("AA"), *node];
-        // starting node has no flow
-        paths_to_explore.insert((total_flow, 0, new_path));
+        paths_to_explore.insert((total_flow, 0, vec![*node]));
     }
 
     for _ in 0..std::i32::MAX {
@@ -80,7 +114,7 @@ pub fn result_a() -> Result<i32, &'static str> {
         if paths_to_explore.len() > cutoff_length {
             paths_to_explore.drain(cutoff_length..);
         }
-        if node_path.len() == 30 {
+        if node_path.len() == 29 {
             return Ok(max_released_volume - total_blocked_volume);
         }
         for node in &node_connections[node_path.last().unwrap()] {
@@ -106,41 +140,11 @@ pub fn result_a() -> Result<i32, &'static str> {
     Err("did not run to completion")
 }
 
-#[allow(dead_code)]
-fn print_char_path(char_path: &Vec<(char, char)>) -> () {
-    for node in char_path.iter() {
-        print!("{}{}-", node.0, node.1);
-    }
-    println!("");
-}
-
 pub fn result_b() -> Result<i32, &'static str> {
-    let input = utils::vector_of_string_vectors_from_file(FILE_PATH);
-    // node name to flow rate and connected nodes
-    let mut node_connections: HashMap<(char, char), Vec<(char, char)>> = HashMap::new();
-    let mut node_flows: HashMap<(char, char), i32> = HashMap::new();
+    let (node_connections, node_flows) = get_input();
     let cutoff_length = 40000;
     let ignore_zero_flow_paths_after_n_iter: i32 = 5;
     let time_until_collapse: usize = 26;
-
-    for line in input.iter() {
-        let node_name = &line[1];
-        let node_name_char1: char = node_name.chars().next().unwrap();
-        let node_name_char2: char = node_name.chars().nth(1).unwrap();
-        let flow_rate: i32 = line[4]
-            .trim_end_matches(';')
-            .trim_start_matches(&['r', 'a', 't', 'e', '='])
-            .parse()
-            .unwrap();
-        let mut connected_node_names: Vec<(char, char)> = Vec::new();
-        for connected_name in 9..line.len() {
-            let char1: char = line[connected_name].chars().next().unwrap();
-            let char2: char = line[connected_name].chars().nth(1).unwrap();
-            connected_node_names.push((char1, char2));
-        }
-        node_flows.insert((node_name_char1, node_name_char2), flow_rate);
-        node_connections.insert((node_name_char1, node_name_char2), connected_node_names);
-    }
 
     let mut total_flow: i32 = 0;
     for flow in node_flows.values() {
